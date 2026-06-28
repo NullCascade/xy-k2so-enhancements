19e4cf567c7813633052752b2cc452ea00c45a2f
NullCascade <NullCascade@gmail.com>
2026-06-28 15:40:10 -0700

    Fix some startup errors if my mod is missing.

--- patches/item_weight.lua
+++ patches/item_weight.lua
@@ -1,30 +1,36 @@
+local function set_item_weight(item, weight_in_kg)
+    local prototype = data.raw.item[item]
+    if (not prototype) then
+        return
+    end
+
+    prototype.weight = weight_in_kg * kg
+end
+
 -- Almost entirely for intermediates
 if settings.startup['xy-item-weight'].value then
     -- Nerf weights
-    data.raw['item']['kr-imersite'].weight = 1 * kg -- this was 10k/rocket which seems odd
+    set_item_weight('kr-imersite', 1) -- this was 10k/rocket which seems odd
     ---- Buff weight
     -- i like the idea of shipping in key intermediate products in buildings to assemble in-situ (other than imersite related)
     -- Some other key intermediates are slightly buffed as well, with electronic components significantly buffed
     -- fuel capacity is also doubled
-    data.raw['item']['kr-rare-metal-ore'].weight = 2 * kg -- this was 70/rocket which is even more odd, moreso a consistency change. still useless because enriched ore is 1kg
-    data.raw['item']['sand'].weight = .5 * kg
-    -- the name on this item is inconsistent
-    if data.raw['item']['glass'] then
-        data.raw['item']['glass'].weight = 1 * kg    
-    end
-    data.raw['item']['steel-plate'].weight = 2 * kg
-    data.raw['item']['kr-imersite-powder'].weight = .5 * kg
-    data.raw['item']['kr-inserter-parts'].weight = .5 * kg
-    data.raw['item']['kr-automation-core'].weight = 1.25 * kg
-    data.raw['item']['kr-electronic-components'].weight = .5 * kg -- key ingredient in red circuits
-    data.raw['item']['kr-iron-beam'].weight = 2 * kg
-    data.raw['item']['kr-steel-beam'].weight = 2 * kg
-    data.raw['item']['kr-fuel'].weight = 2 * kg
-    data.raw['item']['kr-biofuel'].weight = 2 * kg
-    data.raw['item']['kr-advanced-fuel'].weight = 2 * kg
+    set_item_weight('kr-rare-metal-ore', 2) -- this was 70/rocket which is even more odd, moreso a consistency change. still useless because enriched ore is 1kg
+    set_item_weight('sand', .5)
+    set_item_weight('glass', 1)
+    set_item_weight('steel-plate', 2)
+    set_item_weight('kr-imersite-powder', .5)
+    set_item_weight('kr-inserter-parts', .5)
+    set_item_weight('kr-automation-core', 1.25)
+    set_item_weight('kr-electronic-components', .5) -- key ingredient in red circuits
+    set_item_weight('kr-iron-beam', 2)
+    set_item_weight('kr-steel-beam', 2)
+    set_item_weight('kr-fuel', 2)
+    set_item_weight('kr-biofuel', 2)
+    set_item_weight('kr-advanced-fuel', 2)
 
     if mods['Moshine'] then
-        data.raw['item']['silicon-cell'].weight = 2.5 * kg -- i like the ideas of these being more efficient to ship than raw silicon
+        set_item_weight('silicon-cell', 2.5) -- i like the ideas of these being more efficient to ship than raw silicon
     end
 end
 
